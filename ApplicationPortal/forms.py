0327ea527fcc342dd410f4203c41a37a641a0d0c
Varshaa Naganathan <varshaan@gmail.com>
2012-05-03 20:40:39 +0530

    Edited super_home in views & completed the same template
    	Have commented URLs for add, edit, del group.  Will work on them.

--- ApplicationPortal/forms.py
+++ ApplicationPortal/forms.py
@@ -1,16 +1,17 @@
 from django import forms
+from django.forms.widgets import CheckboxSelectMultiple()
 from applications-portal.ApplicationPortal.portal.models import Events
 
-class Loginform(forms.Forms)
+class Loginform(forms.Form):
     username=forms.CharField()
     password=forms.CharField(widget=forms.PasswordInput)
 
-class Preferenceform(forms.Forms)
+class Preferenceform(forms.Form):
     preference1=forms.ModelChoiceField(queryset=Events.objects.all()) #from models they shd send the name of the event alone
     preference2=forms.ModelChoiceField(queryset=Events.objects.all()) 
     preference3=forms.ModelChoiceField(queryset=Events.objects.all()) 
 
-class Registrationform(froms.Forms)
+class Registrationform(forms.Form):
     name=forms.CharField()
     rollnumber=forms.CharField()
     username=forms.CharField()
@@ -20,3 +21,8 @@ class Registrationform(froms.Forms)
     room_number=forms.IntergerField()
     email=forms.EmailField()
     hostel=forms.Charfield()
+
+class Addgroup(form.Form):
+    name=forms.CharField()
+    permissions=forms.MultipleChoiceField(widget=CheckboxSelectMultiple())
+    
