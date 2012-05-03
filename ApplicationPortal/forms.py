cc26e3cdc5548e2a779facbd5dd315a9d9a71101
Janarthanan R <rsdjjana@gmail.com>
2012-05-03 18:01:22 +0530

    forms

--- ApplicationPortal/forms.py
+++ ApplicationPortal/forms.py
@@ -9,3 +9,13 @@ class Preferenceform(forms.Forms)
     preference1=forms.ModelChoiceField(queryset=Events.objects.all()) #from models they shd send the name of the event alone
     preference2=forms.ModelChoiceField(queryset=Events.objects.all()) 
     preference3=forms.ModelChoiceField(queryset=Events.objects.all()) 
+
+class Registrationform(froms.Forms)
+    name=forms.CharField()
+    rollnumber=forms.CharField()
+    username=forms.CharField()
+    Password=forms.CharField(widget=forms.PasswordInput)
+    confirm password=forms.CharField(widget=forms.PasswordInput)
+    cgpa=forms.IntegerField()
+    room number=forms.I
+    email=EmailField()
