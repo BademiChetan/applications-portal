9e4b4a81cc3d605ca68187575d79483a0bd40e01
S.R.Manikandasriram <mani@mani-Inspiron-N5110.(none)>
2012-05-03 19:03:07 +0530

    Edited urls.py
    Added links to the various views

--- ApplicationPortal/urls.py
+++ ApplicationPortal/urls.py
@@ -6,9 +6,10 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'ApplicationPortal.views.home', name='home'),
-    url(r'^login$', 'ApplicationPortal.views.login', name='login'),
-    url(r'^register$', 'ApplicationPortal.views.register', name='register'),
+    url(r'^$', 'ApplicationPortal.portal.views.home', name='home'),
+    url(r'^core/$', 'ApplicationPortal.portal.views.core_home', name='core'),
+    url(r'^coord/$', 'ApplicationPortal.portal.views.coord_home', name='coord'),
+    url(r'^register/$', 'ApplicationPortal.portal.views.register', name='register'),
     # url(r'^ApplicationPortal/', include('ApplicationPortal.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
