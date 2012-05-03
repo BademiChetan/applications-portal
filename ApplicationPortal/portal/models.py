bd5564370c1f85a19fd6525d9c6f712c9d2c3301
Varshaa Naganathan <varshaan@gmail.com>
2012-05-03 18:46:16 +0530

    Function super_user defined in views.py

--- ApplicationPortal/portal/models.py
+++ ApplicationPortal/portal/models.py
@@ -1,4 +1,2 @@
 from django.db import models
-from django.contrib.auth.models import Group
-
 # Create your models here.
--- ApplicationPortal/portal/views.py
+++ ApplicationPortal/portal/views.py
@@ -1,1 +1,15 @@
+from django.contrib.auth.models import Group
+
 # Create your views here.
+
+def super_home(request):
+    """
+    To display super user's home page.  This page will have tables of core details and groups.
+    The super user can add/edit a group and its permissions
+ 
+    """
+    group=Group.objects.all()
+    #Add core object here
+    return render_to_response('/super_home/',locals(),context_instance= RequestContext(request))
+    
+    	
